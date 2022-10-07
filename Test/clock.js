1b5c65c81964cf3e072ad39738a544e3b8f596e0
forourhappy <foroh1504@gmail.com>
2022-10-07 17:48:12 +0900

    연습

---
+++ Test/clock.js
@@ -0,0 +1,18 @@
+const clock = document.querySelector('#clock');
+
+function getClock() {
+  const today = new Date();
+  let hour = today.getHours();
+  const min = String(today.getMinutes()).padStart(2, '0');
+  let daytime = '오전';
+
+  if(hour > 12){
+    daytime = '오후';
+    hour = String(hour-12).padStart(2, '0');
+  }else{
+    daytime = '오전';
+  }
+  clock.innerText = `${daytime} ${hour} : ${min}`
+}
+
+setInterval(getClock, 1000)
